37508af47b5c1c51d76e1aa11085188dccf37956
Victor Mwadime <victormwadime@gmail.com>
2024-10-30 12:53:09 +0300

    route pages

--- express-server.js
+++ express-server.js
@@ -9,8 +9,16 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "contact-me.html"));
+});
+
 app.all("*", (req, res) => {
-  res.status(404).send("Resource not found");
+  res.status(404).sendFile(path.resolve(__dirname, "404.html"));
 });
 
 app.listen(5000, () => {
